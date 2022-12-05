407ef204e730b8e421bf29f141d21b7faaf0bc3f
Kenneth Santos - Tier 2 Admin <t2-adm-santos.8268@teleperformanceusa.com>
2022-12-05 19:59:56 +0800

    Front Page,Login Page, Search page

---
+++ TAP_TALENT_SEARCH_2022/wwwroot/js/site.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+}); 
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('sidebar').classList.add('fixed-side');
+            // add padding top to show content behind navba
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('sidebar').classList.remove('fixed-side');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+}); 
+
+//Filter list
+
+$(document).ready(function () {
+    $("#listSearch").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myList li").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
